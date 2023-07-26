2ba880564751b93abb6f2eb7e9259b0336778eab
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-07-26 13:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// method to log a person with the greeting of name and age
+Person.prototype.greet = function () {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)	
+}
+
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age)
+	this.jobTitle = jobTitle;
+}
 
 // Do not change code below this line
 window.Person = Person;
