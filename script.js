6512a9690fcdfb5c734ac6161007a983b2d86723
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-07-26 13:56:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
   this.age = age;
   // Method to log a person with the greeting of name and age
   this.greet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
   };
 
 }
